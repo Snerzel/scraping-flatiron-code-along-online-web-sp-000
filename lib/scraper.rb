b28672e0c649f27fabd4e50fffe66f6b4d651d71
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-28 23:48:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,12 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
+  end
 
+  def get_courses
+  end
+
+  def make_courses
 
   end
 
